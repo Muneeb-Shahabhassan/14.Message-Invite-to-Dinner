a9eb85237d995af3ef83bcf1c008dfe46327251b
Muneeb-Shahabhassan <muneebsahabhassan098890@gmail.com>
2024-06-06 03:42:49 -0700

    14.MESSAGE INVITE TO DINNER

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let guest_list = [`Shahid`, `Raza`, `Rafay`, `Maroof`, `Hamza`];
+for (let i = 0; i < guest_list.length; i++) {
+    console.log(`Respeted Sir/Madam ` + guest_list[i] + `, \n We invitedd you on dinner tomorrow. \n\n Thanks you`);
+}
---
+++ app.ts
@@ -0,0 +1,6 @@
+let guest_list : string[] =[`Shahid`,`Raza`,`Rafay`,`Maroof`,`Hamza`]
+for (let i = 0; i < guest_list.length; i++) {
+    console.log(`Respeted Sir/Madam ` + guest_list [i] + `, \n We invitedd you on dinner tomorrow. \n\n Thanks you`)
+    
+    
+}
